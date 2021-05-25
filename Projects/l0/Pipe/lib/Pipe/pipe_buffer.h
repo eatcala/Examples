ea9a8325b2f59a11bcfcf4c96899d43b4d41af35
Salem_Luos <thomas.salembier@luos.io>
2021-05-25 15:39:47 +0200

    Add pipe project

---
+++ Projects/l0/Pipe/lib/Pipe/pipe_buffer.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * @file pipe_com
+ * @brief communication driver
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef PIPE_BUFFER_H
+#define PIPE_BUFFER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define PIPE_TO_LUOS_BUFFER_SIZE 1024
+#define LUOS_TO_PIPE_BUFFER_SIZE 512
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+void PipeBuffer_Init(void);
+uint8_t* PipeBuffer_GetP2LBuffer(void);
+uint8_t PipeBuffer_GetP2LTask(uint8_t **data, uint16_t *size);
+void PipeBuffer_AllocP2LTask(uint16_t PositionLastData);
+
+uint8_t* PipeBuffer_GetL2PBuffer(void);
+void PipeBuffer_PutL2PBuffer(uint8_t *data, uint16_t size);
+
+#endif /* PIPE_BUFFER_H */
