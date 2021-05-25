a300c3c0d5efe3af5d5dbb5f62743e81b848ebfa
Nicolas Rabault <nicolas.rabault@luos.io>
2021-05-25 11:15:53 +0200

    [Gate] Monsta cleanup

--- Apps/Gate/gate_config.h
+++ Apps/Gate/gate_config.h
@@ -1,8 +1,19 @@
+/******************************************************************************
+ * @file gate_config
+ * @brief Service gate configuration options
+ * @author Luos
+ ******************************************************************************/
 #ifndef GATE_CONFIG_H
 #define GATE_CONFIG_H
 
+#include "luos.h"
+
 #ifndef JSON_BUFF_SIZE
 #define JSON_BUFF_SIZE 1024
+#endif
+
+#ifndef REFRESH_TIME_S
+#define REFRESH_TIME_S 0.01f
 #endif
 
 // #define GATE_POLLING
