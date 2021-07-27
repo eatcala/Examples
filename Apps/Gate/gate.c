7c17f281d0892f7bafe60e8361b0a812773fdb1d
Salem_Luos <thomas.salembier@luos.io>
2021-07-27 11:24:15 +0200

    apply change on luos type du to luos list change

--- Apps/Gate/gate.c
+++ Apps/Gate/gate.c
@@ -32,7 +32,7 @@ time_luos_t update_time = GATE_REFRESH_TIME_S;
 void Gate_Init(void)
 {
     revision_t revision = {.Major = 1, .Minor = 0, .Build = 0};
-    gate                = Luos_CreateContainer(0, GATE_MOD, "gate", revision);
+    gate                = Luos_CreateContainer(0, GATE_TYPE, "gate", revision);
 }
 
 /******************************************************************************
