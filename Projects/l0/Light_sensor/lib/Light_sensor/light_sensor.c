ab0a677e86d74c8ddf190be33f17b56de2608f11
biliouriv <biliouriv@gmail.com>
2021-08-10 17:30:45 +0200

    LIGHT SENSOR Communication Driver Separation

--- Projects/l0/Light_sensor/lib/Light_sensor/light_sensor.c
+++ Projects/l0/Light_sensor/lib/Light_sensor/light_sensor.c
@@ -12,9 +12,6 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-// Pin configuration
-#define LIGHT_Pin       GPIO_PIN_1
-#define LIGHT_GPIO_Port GPIOA
 
 /*******************************************************************************
  * Variables
@@ -34,73 +31,8 @@ static void LightSensor_MsgHandler(service_t *service, msg_t *msg);
 void LightSensor_Init(void)
 {
     revision_t revision = {.unmap = REV};
-    // ******************* Analog measurement *******************
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    ADC_ChannelConfTypeDef sConfig   = {0};
-
-    // Configure analog input pin channel
-    GPIO_InitStruct.Pin  = LIGHT_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(LIGHT_GPIO_Port, &GPIO_InitStruct);
-
-    // Enable  ADC clocks
-    __HAL_RCC_ADC1_CLK_ENABLE();
-    // Setup Adc to loop on DMA continuously
-    LightSensor_adc.Instance                   = ADC1;
-    LightSensor_adc.Init.ClockPrescaler        = ADC_CLOCK_ASYNC_DIV1;
-    LightSensor_adc.Init.Resolution            = ADC_RESOLUTION_12B;
-    LightSensor_adc.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-    LightSensor_adc.Init.ScanConvMode          = ADC_SCAN_ENABLE;
-    LightSensor_adc.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
-    LightSensor_adc.Init.LowPowerAutoWait      = DISABLE;
-    LightSensor_adc.Init.LowPowerAutoPowerOff  = DISABLE;
-    LightSensor_adc.Init.ContinuousConvMode    = ENABLE;
-    LightSensor_adc.Init.DiscontinuousConvMode = DISABLE;
-    LightSensor_adc.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
-    LightSensor_adc.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    LightSensor_adc.Init.DMAContinuousRequests = ENABLE;
-    LightSensor_adc.Init.Overrun               = ADC_OVR_DATA_PRESERVED;
-    if (HAL_ADC_Init(&LightSensor_adc) != HAL_OK)
-    {
-        Error_Handler();
-    }
-
-    // Add ADC channel to Luos adc configuration.
-    sConfig.Channel      = ADC_CHANNEL_1;
-    sConfig.Rank         = ADC_RANK_CHANNEL_NUMBER;
-    sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
-    if (HAL_ADC_ConfigChannel(&LightSensor_adc, &sConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    // Enable DMA1 clock
-    __HAL_RCC_DMA1_CLK_ENABLE();
-    /* ADC1 DMA Init */
-    /* ADC Init */
-    LightSensor_dma_adc.Instance                 = DMA1_Channel1;
-    LightSensor_dma_adc.Init.Direction           = DMA_PERIPH_TO_MEMORY;
-    LightSensor_dma_adc.Init.PeriphInc           = DMA_PINC_DISABLE;
-    LightSensor_dma_adc.Init.MemInc              = DMA_MINC_ENABLE;
-    LightSensor_dma_adc.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-    LightSensor_dma_adc.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
-    LightSensor_dma_adc.Init.Mode                = DMA_CIRCULAR;
-    LightSensor_dma_adc.Init.Priority            = DMA_PRIORITY_LOW;
-    if (HAL_DMA_Init(&LightSensor_dma_adc) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    // relinik DMA
-    __HAL_LINKDMA(&LightSensor_adc, DMA_Handle, LightSensor_dma_adc);
-
-    // disable DMA Irq
-    HAL_NVIC_DisableIRQ(DMA1_Channel1_IRQn);
-    // Start infinite ADC measurement
-    // Restart DMA
-    HAL_ADC_Start_DMA(&LightSensor_adc, (uint32_t *)analog_input.unmap, sizeof(analog_input.unmap) / sizeof(uint32_t));
-
-    // ******************* Analog measurement *******************
-
+    // **************** communication driver init *************
+    LightSensorCom_Init();
     // ******************* service creation *******************
     Luos_CreateService(LightSensor_MsgHandler, LIGHT_TYPE, "light_sensor", revision);
 }
--- Projects/l0/Light_sensor/lib/Light_sensor/light_sensor_com.h
+++ Projects/l0/Light_sensor/lib/Light_sensor/light_sensor_com.h
@@ -1,18 +1,30 @@
 /******************************************************************************
- * @file light sensor
+ * @file light sensor communication driver
  * @brief driver example a simple light sensor
  * @author Luos
  * @version 0.0.0
  ******************************************************************************/
-#ifndef LIGHT_SENSOR_H
-#define LIGHT_SENSOR_H
-
-#include "luos.h"
-#include "light_sensor_com.h"
+#include "stm32f0xx_hal.h"
+#include "stm32f0xx_ll_usart.h"
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_exti.h"
+#include "stm32f0xx_ll_system.h"
+#include "analog.h"
+#include "main.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+#define LIGHT_Pin       GPIO_PIN_1
+#define LIGHT_GPIO_Port GPIOA
 
+#define LIGHTSENSOR_ADC_CLK()   __HAL_RCC_ADC1_CLK_ENABLE()
+#define LIGHTSENSOR_ADC         ADC1
+#define LIGHTSENSOR_ADC_CHANNEL ADC_CHANNEL_1
+
+#define LIGHTSENSOR_DMA_CLK()   __HAL_RCC_DMA1_CLK_ENABLE()
+#define LIGHTSENSOR_DMA         DMA1
+#define LIGHTSENSOR_DMA_CHANNEL DMA1_Channel1
+#define LIGHTSENSOR_DMA_IRQ     DMA1_Channel1_IRQn
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -20,7 +32,5 @@
 /*******************************************************************************
  * Function
  ******************************************************************************/
-void LightSensor_Init(void);
-void LightSensor_Loop(void);
 
-#endif /* LIGHT_SENSOR_H */
+void LightSensorCom_Init(void);
