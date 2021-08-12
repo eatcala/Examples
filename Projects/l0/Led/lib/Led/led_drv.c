2c08f9950db6cea6c86a58b48612e7190e6124ff
biliouriv <biliouriv@gmail.com>
2021-08-12 14:13:59 +0200

    LED modif driver

--- Projects/l0/Led/lib/Led/led_drv.c
+++ Projects/l0/Led/lib/Led/led_drv.c
@@ -4,9 +4,7 @@
  * @author Luos
  * @version 0.0.0
  ******************************************************************************/
-
 #include "led_drv.h"
-
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -23,8 +21,6 @@ TIM_HandleTypeDef htim3;
 
 void LedTIM2_Init(void);
 void LedTIM3_Init(void);
-void TIM_MspPostInit(TIM_HandleTypeDef *timHandle);
-void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *tim_pwmHandle);
 
 /******************************************************************************
  * @brief Communication init must be called in Led_Init
@@ -50,6 +46,9 @@ void LedTIM2_Init(void)
 {
     TIM_MasterConfigTypeDef sMasterConfig = {0};
     TIM_OC_InitTypeDef sConfigOC          = {0};
+    GPIO_InitTypeDef GPIO_InitStruct      = {0};
+
+    LED_TIM2_CLK();
 
     htim2.Instance               = LED_TIM2_INSTANCE;
     htim2.Init.Prescaler         = 0;
@@ -69,7 +68,17 @@ void LedTIM2_Init(void)
     sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
     HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, LED_TIM2_CHANNEL);
 
-    TIM_MspPostInit(&htim2);
+    LED_TIM2_GPIO_CLK();
+    /**TIM2 GPIO Configuration
+    PA0     ------> TIM2_CH1
+    */
+    GPIO_InitStruct.Pin       = LED_TIM2_CH_PIN;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = LED_AF_TIM2;
+    HAL_GPIO_Init(LED_TIM3_PORT, &GPIO_InitStruct);
+
 }
 /******************************************************************************
  * @brief TIM3 Init
@@ -80,6 +89,9 @@ void LedTIM3_Init(void)
 {
     TIM_MasterConfigTypeDef sMasterConfig = {0};
     TIM_OC_InitTypeDef sConfigOC          = {0};
+    GPIO_InitTypeDef GPIO_InitStruct      = {0};
+
+    LED_TIM3_CLK();
 
     htim3.Instance               = LED_TIM3_INSTANCE;
     htim3.Init.Prescaler         = 0;
@@ -101,70 +113,27 @@ void LedTIM3_Init(void)
 
     HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, LED_TIM3_CHANNEL2);
 
-    TIM_MspPostInit(&htim3);
-}
-/******************************************************************************
- * @brief TIM pins driver initilization
- * @param None
- * @return None
- ******************************************************************************/
-void TIM_MspPostInit(TIM_HandleTypeDef *timHandle)
-{
-
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    if (timHandle->Instance == LED_TIM2_INSTANCE)
-    {
-        LED_TIM2_GPIO_CLK();
-        /**TIM2 GPIO Configuration
-    PA0     ------> TIM2_CH1
-    */
-        GPIO_InitStruct.Pin       = LED_TIM2_CH_PIN;
-        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
-        GPIO_InitStruct.Alternate = LED_AF_TIM2;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    }
-    else if (timHandle->Instance == LED_TIM3_INSTANCE)
-    {
-
-        LED_TIM3_GPIO_CLK();
+    LED_TIM3_GPIO_CLK();
         /**TIM3 GPIO Configuration
     PB4     ------> TIM3_CH1
     PB5     ------> TIM3_CH2
     */
-        GPIO_InitStruct.Pin       = LED_TIM3_CH1_PIN | LED_TIM3_CH2_PIN;
-        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
-        GPIO_InitStruct.Alternate = LED_AF_TIM3;
-        HAL_GPIO_Init(LED_TIM3_PORT, &GPIO_InitStruct);
-    }
+    GPIO_InitStruct.Pin       = LED_TIM3_CH1_PIN | LED_TIM3_CH2_PIN;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = LED_AF_TIM3;
+    HAL_GPIO_Init(LED_TIM3_PORT, &GPIO_InitStruct);
 }
-
 /******************************************************************************
- * @brief clocks initialization
- * @param None
- * @return None
+ * @brief Led driver write function puts the color to the led
+ * @param rgb value
+ * @return error value (succeed/failed)
  ******************************************************************************/
-void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *tim_pwmHandle)
-{
-
-    if (tim_pwmHandle->Instance == LED_TIM2_INSTANCE)
-    {
-        __HAL_RCC_TIM2_CLK_ENABLE();
-    }
-    else if (tim_pwmHandle->Instance == LED_TIM3_INSTANCE)
-    {
-        __HAL_RCC_TIM3_CLK_ENABLE();
-    }
-}
-
-uint8_t LedDrv_Write(color_t* rgb)
+uint8_t LedDrv_Write(color_t *rgb)
 {
     LED_TIM3_INSTANCE->CCR1 = (uint16_t)rgb->r * 10;
     LED_TIM3_INSTANCE->CCR2 = (uint16_t)rgb->g * 10;
     LED_TIM2_INSTANCE->CCR1 = (uint16_t)rgb->b * 10;
-
     return 0;
 }
--- Projects/l0/Led/lib/Led/led_drv.h
+++ Projects/l0/Led/lib/Led/led_drv.h
@@ -14,19 +14,23 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+#define LED_TIM2_CLK()    __HAL_RCC_TIM2_CLK_ENABLE();
+#define LED_TIM2_INSTANCE TIM2
+#define LED_TIM2_CHANNEL  TIM_CHANNEL_1
+
 #define LED_TIM2_GPIO_CLK() __HAL_RCC_GPIOA_CLK_ENABLE()
-#define LED_TIM2_INSTANCE   TIM2
 #define LED_TIM2_PORT       GPIOA
-#define LED_TIM2_CHANNEL    TIM_CHANNEL_1
 #define LED_TIM2_CH_PIN     GPIO_PIN_0
 #define LED_AF_TIM2         GPIO_AF2_TIM2
 
+#define LED_TIM3_CLK()    __HAL_RCC_TIM3_CLK_ENABLE();
+#define LED_TIM3_INSTANCE TIM3
+#define LED_TIM3_CHANNEL1 TIM_CHANNEL_1
+#define LED_TIM3_CHANNEL2 TIM_CHANNEL_2
+
 #define LED_TIM3_GPIO_CLK() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define LED_TIM3_INSTANCE   TIM3
 #define LED_TIM3_PORT       GPIOB
-#define LED_TIM3_CHANNEL1   TIM_CHANNEL_1
 #define LED_TIM3_CH1_PIN    GPIO_PIN_4
-#define LED_TIM3_CHANNEL2   TIM_CHANNEL_2
 #define LED_TIM3_CH2_PIN    GPIO_PIN_5
 #define LED_AF_TIM3         GPIO_AF1_TIM3
 
